09e7786bf620ed09c1498526f91541c7468353b1
Yuri Rodrigues <yurihenriques2004@gmail.com>
2026-03-02 00:33:18 +0100

    update

--- models.py
+++ models.py
@@ -1,8 +1,82 @@
-from sqlalchemy import Column,ForeignKey, Float, Boolean, Integer, String,create_engine
-from sqlalchemy.orm import relationship, declarative_base
+from sqlalchemy import Column, ForeignKey, Float, DateTime, Integer, String, create_engine, JSON, Boolean
+from sqlalchemy.orm import declarative_base
 
 db = create_engine('sqlite:///data.db')
 
 base = declarative_base()
 
 
+class Book(base):
+    __tablename__ = 'books'
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    name = Column("name", String, nullable=False)
+    writer = Column("writer", String, nullable=False)
+    Category = Column("Category", String, nullable=False)
+    isbn = Column("isbn", String, nullable=False)
+    stock = Column("stock", Integer, nullable=False)
+
+    def __init__(self, name, writer, Category, isbn, stock):
+        self.name = name
+        self.writer = writer
+        self.Category = Category
+        self.isbn = isbn
+        self.stock = stock
+
+
+class User(base):
+    __tablename__ = 'users'
+
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    name = Column("Name", String)
+    email = Column("email", String, nullable=False)
+    password = Column("password", String, nullable=False)
+    phone = Column("phone", String, nullable=False)
+    id_card = Column("id_card", String, nullable=False)
+    loan_history = Column("loan_history", JSON, default=[])
+
+    def __init__(self, name, email, password, phone, id_card):
+        self.name = name
+        self.email = email
+        self.password = password
+        self.phone = phone
+        self.id_card = id_card
+        self.loan_history = []
+
+
+class Loan(base):
+    __tablename__ = 'loans'
+
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    book = Column("book_id", Integer, ForeignKey('books.id'))
+    user = Column("user", Integer, ForeignKey('users.id'))
+    loan_date = Column("loan_date", DateTime)
+    expected_return_date = Column("expected_return_date", DateTime)
+    actual_return_date = Column("actual_return_date", DateTime)
+    active = Column("active", Boolean)
+
+    def __init__(self, book, user, loan_date, expected_return_date, actual_return_date):
+        self.book = book
+        self.user = user
+        self.loan_date = loan_date
+        self.expected_return_date = expected_return_date
+        self.actual_return_date = actual_return_date
+
+
+class Fine(base):
+    __tablename__ = 'fines'
+
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    book = Column("book_id", Integer, ForeignKey('books.id'))
+    user = Column("user", Integer, ForeignKey('users.id'))
+    date = Column("date", DateTime)
+    fine = Column("fine", Float)
+    active = Column("active", Boolean)
+
+    def __init__(self, book, user, date, fine=2.0):
+        self.book = book
+        self.user = user
+        self.date = date
+        self.fine = fine
+
+
+base.metadata.create_all(bind=db)
